35e164875ef2692de814f59d83015492357fae44
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-07-17 15:08:52 +0530

    feat: create shared Auth.js configuration

---
+++ src/auth.config.ts
@@ -0,0 +1,31 @@
+import Google from "next-auth/providers/google";
+
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+
+  pages: {
+    signIn: "/login",
+  },
+
+  callbacks: {
+    authorized({ auth, request }) {
+      const isLoggedIn = !!auth?.user;
+
+      const isDashboard =
+        request.nextUrl.pathname.startsWith("/dashboard");
+
+      if (isDashboard && !isLoggedIn) {
+        return false;
+      }
+
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
